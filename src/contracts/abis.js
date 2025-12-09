2787342ab1d88e6daec7f41d2112f79051be78af
aarongfil <aaron.garcia@rai.usc.es>
2025-12-09 17:34:24 +0100

    Commit inicial - Base proyecto para ticketGO

---
+++ src/contracts/abis.js
@@ -0,0 +1,7 @@
+import ipfsStorage from "./abis/IPFS_Tickets.json";
+
+const abis = {
+  ipfs: ipfsStorage,
+};
+
+export default abis;
---
+++ src/contracts/addresses.js
@@ -0,0 +1,5 @@
+const addresses = {
+  ipfs: "0x717D538592864bEd70e1C2D7CbF63F6FCbEf6f9B",
+};
+
+export default addresses;
